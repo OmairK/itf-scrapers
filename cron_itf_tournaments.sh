f593fdedeb6cf12ae3483063f0e3dad62a912f38
omair_server <omairkhan064@gmail.com>
2019-11-23 15:31:24 +0000

    Fix (itf scraper bug fix(date dynamic))

--- cron_itf_tournaments.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ~/scrapervenv/bin/activate && export PATH=$PATH:/home/fsc/itf-scrapers
-cd ~/itf-scrapers
-chmod +x load_itf_tournaments.sh 
-./load_itf_tournaments.sh 
--- paymentCheck.py
+++ paymentCheck.py
@@ -5,7 +5,7 @@ from datetime import timedelta as td
 conn = psycopg2.connect(database="fsc_database", user="postgres",
                         host='127.0.0.1', password="test123", port=5432)
 cur = conn.cursor()
-cur.execute("DELETE FROM payments_payment WHERE two_way_auth = 'O' AND date_time_of_payment+interval'600 seconds'> now();")
+cur.execute("DELETE FROM payments_payment WHERE two_way_auth = 'O' AND date_time_of_payment+interval'600 seconds'< now();")
 conn.commit()
 
 cur.close()
