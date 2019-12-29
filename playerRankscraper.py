eaa5a30c2a500fbfe7a3d30792f41564effb4b66
Omair Khan <omairkhan064@gmail.com>
2019-12-30 04:42:49 +0530

    (Fix) Fixed the bug in the updated rank scraper

--- playerRankscraper.py
+++ playerRankscraper.py
@@ -3,12 +3,10 @@ import random
 import requests
 import os
 import json
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 options = FirefoxOptions()
@@ -16,38 +14,31 @@ options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
 
 
-"""
-Procedure
-    1) Generate URls for all age groups
-"""
-
-itf_to_local_dict = {'rank': 'Rank', 'playerfamilyname': 'Player', 'rankmovement': 'Movement',
-                     'birthYear': 'DOB', 'tournamentsplayed': 'Events', 'points': 'Points', 'Age Group': 'Age Group',
-                     'Type': 'Type'}
-
-local_player_list = []
-
 def get_urls():
     """
-    Generate urls for the url list
+    Generate urls for scraping the xml data and passes the page source
+    into xml_scraper
     """
-
+    all_players = []
     categories = ['S', 'D']
     for category in categories:
-        for age in range(35, 71, 5):
-            randoms = random.randrange(51, 99)
-            url = f"https://www.itftennis.com/Umbraco/Api/PlayerRankApi/GetPlayerRankings?circuitCode=VT&playerTypeCode=M&matchTypeCode=S&ageCategoryCode=V35&nationCode=IND%20%20%20%20%20&take={randoms}&skip=0"
+        for age in range(35, 85, 5):
+            randoms = random.randrange(70, 99)
+            url = f"https://www.itftennis.com/Umbraco/Api/PlayerRankApi/GetPlayerRankings?circuitCode=VT&playerTypeCode=M&matchTypeCode=S&ageCategoryCode=V{age}&nationCode=IND%20%20%20%20%20&take={randoms}&skip=0"
             driver.get(url)
-            xml_scraper(age, category, driver.page_source)
+            all_players.append(xml_scraper(
+                age, category, driver.page_source))
+    return all_players
 
 
 def xml_scraper(age, category, xml_txt):
-
-    large_list = []
+    """
+    Scrapes the details of each player from xml data and puts it in a list
+    """
+    players_w_same_category = []
     xml_txt = BeautifulSoup(xml_txt, 'lxml')
-
     players = xml_txt.find_all('playerrankingapimodel')
-    print(players)
+
     for player in players:
         loc_player = {'Rank': None, 'Player': None, 'Movement': None, 'Nation': 'IND',
                       'DOB': None, 'Events': None, 'Points': None, 'Age Group': age, 'Type': category}
@@ -58,19 +49,21 @@ def xml_scraper(age, category, xml_txt):
         loc_player['Movement'] = player.find('rankmovement').text
         loc_player['Events'] = player.find('tournamentsplayed').text
         loc_player['Points'] = player.find('points').text
-        local_player_list.append(loc_player)
-        print(json.dumps(local_player_list))
-        
+
+        players_w_same_category.append(loc_player)
+
+    return players_w_same_category
 
 
 def driver_code():
-    get_urls()
-    
-    with open('PlayerRanks.json','w') as file:
-        file.write(json.dumps(local_player_list))
+    """
+    Main code block
+    """
+    player_dict = get_urls()
+
+    with open('PlayerRanks.json', 'w') as file:
+        file.write(json.dumps(player_dict))
 
 
-
-    
-
-driver_code()
+if __name__ == '__main__':
+    driver_code()
